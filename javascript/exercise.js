3d1d8d5a6bf5bb1ee641db292a71528ed3f6c23c
BlairMcMillan <theblairmcmillan@gmail.com>
2016-01-11 20:02:27 -0600

    adding all html & css exercises thus far

---
+++ javascript/exercise.js
@@ -0,0 +1,44 @@
+var phrase = "Hey, look at me";
+
+var element = document.getElementsByClassName("output");
+console.log("element.innerHTML");
+
+
+element.idone = phrase;
+console.log("element, element");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
